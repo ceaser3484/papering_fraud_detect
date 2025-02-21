85bf7ea2d0f0f41f5f8ce4ff59ac87455cac18b6
Changdae Jang <ceaser3484@gmail.com>
2025-02-21 15:12:20 +0900

    gitignore 추가, 안내 사항 추가

---
+++ train_main.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+import numpy as np
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import yaml
+
+def preprocessing():
+    pass
+
+
+
+def main():
+    print('hello')
+
+
+if __name__ == '__main__':
+    main()
