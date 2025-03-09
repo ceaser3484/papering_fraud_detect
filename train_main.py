4c651e6cb2d8828e0986ac91a0ce7e6866075518
Lee Minjeong <leemiinjeong@iminjeong-ui-MacBookAir.local>
2025-03-09 11:59:32 +0900

    수정

--- train_main.py
+++ train_main.py
@@ -15,17 +15,16 @@ train_data_path = os.path.join(DATA_PATH, 'dataset', 'train')
 
 # 증강
 train_datagen = ImageDataGenerator(
-    rescale = 1./255,
-    rotation_range = 20,  
-    width_shift_range = 0.2,  
-    height_shift_range = 0.2,  
-    shear_range = 0.2,  
-    zoom_range = 0.2, 
-    horizontal_flip = True,
-    brightness_range = [0.8, 1.2], 
-    fill_mode='nearest'
+    rescale=1./255,
+    rotation_range=30,
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    shear_range=0.3,
+    zoom_range=(0.8, 1.2),  
+    horizontal_flip=True,
+    brightness_range=(0.7, 1.4),  
+    fill_mode='reflect'  
 )
-
 # load data
 train_generator = train_datagen.flow_from_directory(
     train_data_path,
@@ -37,29 +36,57 @@ train_generator = train_datagen.flow_from_directory(
 num_classes = len(train_generator.class_indices)
 print(f'총 {num_classes}개의 클래스: {train_generator.class_indices}')
 
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-base_model.trainable = False  # Feature Extractor 역할
+# fine_tuning
+use_transfer_learning = True
 
-model = Sequential([
-    base_model,
-    GlobalAveragePooling2D(), 
-    Dense(256, activation='relu'),
-    BatchNormalization(),
-    Dropout(0.3),
-    Dense(num_classes, activation='softmax')
-])
+if use_transfer_learning:
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    base_model.trainable = False  # 처음에는 고정
 
-optimizer = Adam(learning_rate=0.0001)  
+    model = Sequential([
+        base_model,
+        Flatten(),
+        Dense(256, activation='relu'),
+        BatchNormalization(),  
+        Dropout(0.4), 
+        Dense(num_classes, activation='softmax')
+    ])
+else:
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)),
+        BatchNormalization(),  
+        MaxPooling2D(2, 2),
+        
+        Conv2D(64, (3, 3), activation='relu', padding='same'),
+        BatchNormalization(), 
+        MaxPooling2D(2, 2),
+        
+        Conv2D(128, (3, 3), activation='relu', padding='same'),
+        BatchNormalization(),  
+        MaxPooling2D(2, 2),
 
-# compile
+        Flatten(),
+        Dense(256, activation='relu'),
+        Dropout(0.4), 
+        Dense(num_classes, activation='softmax')
+    ])
+
+learning_rate = 5e-4  
+
+optimizer = Adam(learning_rate=learning_rate)
+
 model.compile(
     optimizer=optimizer,
-    loss='categorical_crossentropy', 
+    loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-# callback
-lr_reduction = ReduceLROnPlateau(monitor='loss', patience=3, factor=0.5, min_lr=1e-6)
+# Fine-Tuning (20번째 epoch부터 몇 개의 레이어 해제)
+for layer in base_model.layers[-4:]:
+    layer.trainable = True
+
+# Callback
+lr_reduction = ReduceLROnPlateau(monitor='loss', patience=3, factor=0.7, min_lr=1e-6)
 early_stopping = EarlyStopping(monitor='loss', patience=5, restore_best_weights=True)
 
 # 학습
@@ -69,30 +96,31 @@ history = model.fit(
     callbacks=[lr_reduction, early_stopping]
 )
 
-# save
+# 모델 저장
 model_save_path = os.path.join(DATA_PATH, 'cnn_model_fixed.h5')
 model.save(model_save_path)
-print(f'모델 학습 및 저장 완료: {model_save_path}')
+print(f'모델 학습 및 저장 완료 : {model_save_path}')
 
 def plot_training(history):
     acc = history.history['accuracy']
     loss = history.history['loss']
+    
     epochs = range(len(acc))
-
+    
     plt.figure(figsize=(10, 4))
-
+    
     # 정확도
     plt.subplot(1, 2, 1)
     plt.plot(epochs, acc, 'b', label='Training Accuracy')
     plt.title('Training Accuracy')
     plt.legend()
-
-    # 손실값
+    
+    # loss 
     plt.subplot(1, 2, 2)
     plt.plot(epochs, loss, 'r', label='Training Loss')
     plt.title('Training Loss')
     plt.legend()
-
+    
     plt.show()
-
+    
 plot_training(history)
